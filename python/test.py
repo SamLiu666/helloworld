860a2ccf0d9efe760added66a17d7e08c045113a
SamLiu666 <aa2083683710@gmail.com>
2020-02-29 17:45:28 +0800

    newwork programming

--- python/test.py
+++ python/test.py
@@ -1,48 +1,15 @@
-from multiprocessing import Process, Queue
-from random import randint
-from time import time
+from socket import socket
 
 
-def task_handler(curr_list, result_queue):
-    total = 0
-    for number in curr_list:
-        total += number
-    result_queue.put(total)
-def main1():
-    total = 0
-    number_list = [x for x in range(1, 10000000)]
-    start = time()
-    for number in number_list:
-        total += number
-    print(total)
-    end = time()
-    print('Execution time: %.3fs' % (end - start))
-
-def main2():
-    processes = []
-    number_list = [x for x in range(1, 10000000)]
-    result_queue = Queue()
-    index = 0
-    # 启动8个进程将数据切片后进行运算
-    for _ in range(8):
-        p = Process(target=task_handler,
-                    args=(number_list[index:index + 10000000], result_queue))
-        index += 10000000
-        processes.append(p)
-        p.start()
-    # 开始记录所有进程执行完成花费的时间
-    start = time()
-    for p in processes:
-        p.join()
-    # 合并执行结果
-    total = 0
-    while not result_queue.empty():
-        total += result_queue.get()
-    print(total)
-    end = time()
-    print('Execution time: ', (end - start), 's', sep='')
+def main():
+    # 1.创建套接字对象默认使用IPv4和TCP协议
+    client = socket()
+    # 2.连接到服务器(需要指定IP地址和端口)
+    client.connect(('192.168.1.107', 6789))
+    # 3.从服务器接收数据
+    print(client.recv(1024).decode('utf-8'))
+    client.close()
 
 
 if __name__ == '__main__':
-    main1()
-    main2()
+    main()
