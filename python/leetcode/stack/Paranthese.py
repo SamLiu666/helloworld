985634d001b9370c7c53eb02189e5fd1cd246053
SamLiu666 <aa2083683710@gmail.com>
2020-03-09 22:51:49 +0800

    add description

--- python/leetcode/stack/Paranthese.py
+++ python/leetcode/stack/Paranthese.py
@@ -1,3 +1,9 @@
+from matplotlib import collections
+"""1、有效括号问题
+2、用栈实现队列，FIFO，用两个栈实现
+3、用队列实现栈，FILO，用collections 库实现
+4、有效括号"""
+
 class solution:
 
     def pro_1021(self, s):
@@ -20,6 +26,65 @@ class solution:
                 res.append(char)
             flag += 1 if char == '(' else -1
         return "".join(res)
+
+
+class Myqueue:
+    """栈实现队列,FIFO，用两个栈"""
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x:int):
+        # 入栈
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+    def pop(self):
+        # 出栈
+
+        return self.stack1.pop()
+
+    def peek(self):
+        # 得到最开始的数据
+        return self.stack1[-1]
+
+    def empty(self):
+        return not self.stack1
+
+
+class Mystack:
+    """用队列实现栈"""
+    def __init__(self):
+        self.stack = collections.deque([])
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        self.stack.append(x)
+
+    # @return nothing
+    def pop(self):
+        for i in range(len(self.stack) - 1):
+            self.stack.append(self.stack.popleft())
+
+        self.stack.popleft()
+
+    # @return an integer
+    def top(self):
+        return self.stack[-1]
+
+    # @return an boolean
+    def empty(self):
+        return len(self.stack) == 0
+
+
+class problem:
+
+    def pro_20(self, s):
+
 
 
 s = solution()
