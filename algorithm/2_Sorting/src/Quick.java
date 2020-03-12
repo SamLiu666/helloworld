5752c46dfc95f7c6574c2cfba256c49960449a17
SamLiu666 <aa2083683710@gmail.com>
2020-03-12 23:56:29 +0800

    quick sort

---
+++ algorithm/2_Sorting/src/Quick.java
@@ -0,0 +1,54 @@
+public class Quick {
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    private static void printA(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        System.out.println('\n');
+    }
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+    public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1); // 递归调用自身
+        printA(a);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        // 快速排序，算法复杂度 2nlgn
+        int j = partition(a, lo, hi); // 以j 左侧的元素都小于 a[j]，j 右侧的元素都大于 a[j]
+        // 从顶往下切分数组，完成排序
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        // 实现 j 位置出，左边都小于a[j]，右边都大于a[j]
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo]; //初始化一个被比较值v，默认为第一个元素
+        while (true) {
+            //一个字串不动，到达边界了，完成交叉，结束循环，交换位置
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;   // 若i,j交叉，则结束循环
+            // 交换a[i],a[j] 的位置
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    public static void main(String[] args) {
+        Comparable[] a = {2, 4, 5, 3, 1, 11, 7};
+        Comparable[] s = {'a', 'm', 'z', 'c', 'b'};
+        printA(a);printA(s);
+        sort(a);sort(s);
+    }
+}
