9237eeadcf42d5c3c08844b915e1e1c3d1dcbfd3
SamLiu666 <aa2083683710@gmail.com>
2020-03-17 22:59:08 +0800

    leetcode backwards

---
+++ python/leetcode/DP_greedy_back_etc/back.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+class solution:
+
+    # 46 DFS求排列组合
+    def permute(self, nums):
+        res =[]
+        self.dfs(nums, [], res)
+        return res
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            print(res)
+            # 返回之前的路径
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+
+    def permute_2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.helper(nums, 0, result)
+        return result
+    def helper(self, nums, begin, result):
+        n = len(nums)
+        if begin == n:
+            tmp = nums[:]
+            result.append(tmp)
+            return
+
+        for i in range(begin, n):
+            nums[begin], nums[i] = nums[i], nums[begin]
+            self.helper(nums, begin + 1, result)
+            nums[begin], nums[i] = nums[i], nums[begin]
+
+    # 22 生成括号，回溯算法
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def back(s = '', left=0, right=0):
+            if(len(s))==2*n:
+                res.append(s)
+                return
+            if left<n:  back(s+'(', left+1, right)
+            if right<left: back(s+')', left, right+1)
+        back()
+        return res
+
+s = solution()
+nums = [1,2,3]
+print(s.permute_2(nums), s.generateParenthesis(3), sep='\n')
