75ba10b42b5c6090a9b6bf5d672a5e09fbdfc6be
SamLiu666 <aa2083683710@gmail.com>
2020-04-10 10:17:58 +0800

    个人项目

--- MachineLearning/tensorflow/2 核心概念.py
+++ MachineLearning/tensorflow/2 核心概念.py
@@ -7,3 +7,4 @@ i = tf.constant(1) # tf.int32 类型常量
 # d = tf.constant(3.14,dtype = tf.double) # tf.double 类型常量
 # s = tf.constant("hello world") # tf.string类型常量
 # b = tf.constant(True) #tf.bool类型常量
+print(i)
