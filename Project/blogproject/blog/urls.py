e16bd4f2a60beaf2a78269ed07d76a08787dde85
SamLiu666 <aa2083683710@gmail.com>
2020-04-14 23:22:22 +0800

    my blog

--- Project/blogproject/blog/urls.py
+++ Project/blogproject/blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+path('posts/<int:pk>/', views.detail, name='detail'),
+    # path('posts/<int:pk>/', views.detail, name='detail')
 ]
